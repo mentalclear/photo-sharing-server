f51f961d9c757cea2b28047fa7106398dc117ba8
Dmitrii Kilishek <mentalclear@gmail.com>
2022-01-20 10:26:59 -0500

    Continue with 4.7 at 22:30

--- src/db/index.js
+++ src/db/index.js
@@ -2,3 +2,7 @@ export { db } from './db';
 export { getPhotosForUser } from './getPhotosForUser';
 export { getSharedPhotosForUser } from './getSharedPhotosForUser';
 export { addNewPhoto } from './addNewPhoto';
+export { getPopulatedPhoto } from './getPopulatedPhoto';
+export { getSanitizedPhoto } from './getSanitizedPhoto';
+export { getUserOwnsPhoto } from './getUserOwnsPhoto';
+export { getPhotoIsSharedWithUser } from './getPhotoIsSharedWithUser';
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { getMyPhotosRoute } from './getMyPhotosRoute';
 import { getSharedWithMePhotosRoute } from './getSharedWithMePhotosRoute';
 import { uploadPhotoRoute } from './uploadPhotoRoute';
+import { getPhotoRoute } from './getPhotoRoute';
 
 export { protectRouteMiddleware } from "./protectRouteMiddleware";
 export const routes = [
     getMyPhotosRoute,
     getSharedWithMePhotosRoute,
     uploadPhotoRoute,
+    getPhotoRoute,
 ];
